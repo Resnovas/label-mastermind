0e04977aa2f1ea8035eacc60a20a092e2331ee14
Restyled.io <commits@restyled.io>
2020-11-05 18:49:14 +0000

    Restyled by prettier

--- dist/__tests__/Utils.test.js
+++ dist/__tests__/Utils.test.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 var __awaiter =
   (this && this.__awaiter) ||
   function (thisArg, _arguments, P, generator) {
@@ -6,40 +6,40 @@ var __awaiter =
       return value instanceof P
         ? value
         : new P(function (resolve) {
-            resolve(value)
-          })
+            resolve(value);
+          });
     }
     return new (P || (P = Promise))(function (resolve, reject) {
       function fulfilled(value) {
         try {
-          step(generator.next(value))
+          step(generator.next(value));
         } catch (e) {
-          reject(e)
+          reject(e);
         }
       }
       function rejected(value) {
         try {
-          step(generator['throw'](value))
+          step(generator['throw'](value));
         } catch (e) {
-          reject(e)
+          reject(e);
         }
       }
       function step(result) {
         result.done
           ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected)
+          : adopt(result.value).then(fulfilled, rejected);
       }
-      step((generator = generator.apply(thisArg, _arguments || [])).next())
-    })
-  }
-Object.defineProperty(exports, '__esModule', { value: true })
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 const settings = {
   labels: {
     test: {
       name: 'test',
       color: 'CFD3D7',
-      description: 'This issue / pull request has been abandon'
-    }
+      description: 'This issue / pull request has been abandon',
+    },
   },
   issue: {
     bug: {
@@ -47,14 +47,14 @@ const settings = {
       conditions: [
         {
           type: 'titleMatches',
-          pattern: '/^bug(\\(.*\\))?:/i'
+          pattern: '/^bug(\\(.*\\))?:/i',
         },
         {
           type: 'descriptionMatches',
-          pattern: '/(created|new|opened|made)( an| a)? bug/i'
-        }
-      ]
-    }
+          pattern: '/(created|new|opened|made)( an| a)? bug/i',
+        },
+      ],
+    },
   },
   pr: {
     bug: {
@@ -62,21 +62,21 @@ const settings = {
       conditions: [
         {
           type: 'titleMatches',
-          pattern: '/^bug(\\(.*\\))?:/i'
+          pattern: '/^bug(\\(.*\\))?:/i',
         },
         {
           type: 'descriptionMatches',
-          pattern: '/(created|new|opened|made)( an| a)? bug/i'
-        }
-      ]
-    }
-  }
-}
+          pattern: '/(created|new|opened|made)( an| a)? bug/i',
+        },
+      ],
+    },
+  },
+};
 test('test colour format', () =>
   __awaiter(void 0, void 0, void 0, function* () {
-    yield expect(formatColour(settings.labels.test.color)).toBe('CFD3D7')
-  }))
+    yield expect(formatColour(settings.labels.test.color)).toBe('CFD3D7');
+  }));
 test('test colour format', () =>
   __awaiter(void 0, void 0, void 0, function* () {
-    yield expect(formatColour(`#${settings.labels.test.color}`)).toBe('CFD3D7')
-  }))
+    yield expect(formatColour(`#${settings.labels.test.color}`)).toBe('CFD3D7');
+  }));
--- dist/src/api/getLabels.js
+++ dist/src/api/getLabels.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 var __awaiter =
   (this && this.__awaiter) ||
   function (thisArg, _arguments, P, generator) {
@@ -6,42 +6,42 @@ var __awaiter =
       return value instanceof P
         ? value
         : new P(function (resolve) {
-            resolve(value)
-          })
+            resolve(value);
+          });
     }
     return new (P || (P = Promise))(function (resolve, reject) {
       function fulfilled(value) {
         try {
-          step(generator.next(value))
+          step(generator.next(value));
         } catch (e) {
-          reject(e)
+          reject(e);
         }
       }
       function rejected(value) {
         try {
-          step(generator['throw'](value))
+          step(generator['throw'](value));
         } catch (e) {
-          reject(e)
+          reject(e);
         }
       }
       function step(result) {
         result.done
           ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected)
+          : adopt(result.value).then(fulfilled, rejected);
       }
-      step((generator = generator.apply(thisArg, _arguments || [])).next())
-    })
-  }
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.getLabels = void 0
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.getLabels = void 0;
 exports.getLabels = ({ client, repo }) =>
   __awaiter(void 0, void 0, void 0, function* () {
     const labels = yield client.issues.listLabelsForRepo(
-      Object.assign({}, repo)
-    )
-    return labels.data.map(label => ({
+      Object.assign({}, repo),
+    );
+    return labels.data.map((label) => ({
       name: label.name,
       description: label.description,
-      color: label.color
-    }))
-  })
+      color: label.color,
+    }));
+  });
