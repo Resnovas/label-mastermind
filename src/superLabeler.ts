b1f1ce732adadd66f91ca6ebcd069a67527d6d43
Jonathan Stevens <jonathan@videndum.studio>
2020-11-12 03:20:36 +0000

    chore: remove catch

--- src/superLabeler.ts
+++ src/superLabeler.ts
@@ -43,144 +43,134 @@ export default class SuperLabeler {
    * @since 1.0.0
    */
   async run() {
-    try {
-      const configJSON = this.opts.configJSON
-      const configPath = this.opts.configPath
-      const dryRun = this.opts.dryRun
-      const repo: { repo: string; owner: string } = {
-        owner: process.env.GITHUB_REPOSITORY_OWNER || context.repo.owner,
-        repo: process.env.GITHUB_REPOSITORY || context.repo.repo
+    const configJSON = this.opts.configJSON
+    const configPath = this.opts.configPath
+    const dryRun = this.opts.dryRun
+    const repo: { repo: string; owner: string } = {
+      owner: process.env.GITHUB_REPOSITORY_OWNER || context.repo.owner,
+      repo: process.env.GITHUB_REPOSITORY || context.repo.repo
+    }
+
+    /**
+     * Capture and log context to debug for Local Running
+     * @author TGTGamer
+     * @since 1.0.0
+     */
+
+    log(
+      `Context for local running. See readme.md for information on how to setup local running: ${JSON.stringify(
+        context
+      )}`,
+      1
+    )
+
+    /**
+     * Get the configuration
+     * @author IvanFon, TGTGamer, jbinda
+     * @since 1.0.0
+     */
+    let config: Config
+    if (!configJSON) {
+      if (!fs.existsSync(configPath)) {
+        throw new Error(`config not found at "${configPath}"`)
       }
+      config = await JSON.parse(fs.readFileSync(configPath).toString())
+      log(`Config: ${JSON.stringify(config)}`, 1)
+    } else {
+      config = configJSON
+    }
 
-      /**
-       * Capture and log context to debug for Local Running
-       * @author TGTGamer
-       * @since 1.0.0
-       */
+    /**
+     * Handle the context
+     * @author IvanFon, TGTGamer, jbinda
+     * @since 1.0.0
+     */
+    let curContext:
+      | { type: 'pr'; context: PRContext }
+      | { type: 'issue'; context: IssueContext }
 
-      log(
-        `Context for local running. See readme.md for information on how to setup local running: ${JSON.stringify(
-          context
-        )}`,
-        1
-      )
-
-      /**
-       * Get the configuration
-       * @author IvanFon, TGTGamer, jbinda
-       * @since 1.0.0
-       */
-      let config: Config
-      if (!configJSON) {
-        if (!fs.existsSync(configPath)) {
-          throw new Error(`config not found at "${configPath}"`)
-        }
-        config = await JSON.parse(fs.readFileSync(configPath).toString())
-        log(`Config: ${JSON.stringify(config)}`, 1)
-      } else {
-        config = configJSON
+    if (context.payload.pull_request) {
+      const ctx = await contextHandler.parsePR(context, this.client, repo)
+      if (!ctx) {
+        throw new Error('pull request not found on context')
       }
-
-      /**
-       * Handle the context
-       * @author IvanFon, TGTGamer, jbinda
-       * @since 1.0.0
-       */
-      let curContext:
-        | { type: 'pr'; context: PRContext }
-        | { type: 'issue'; context: IssueContext }
-
-      if (context.payload.pull_request) {
-        const ctx = await contextHandler.parsePR(context, this.client, repo)
-        if (!ctx) {
-          throw new Error('pull request not found on context')
-        }
-        log(`PR context: ${JSON.stringify(ctx)}`, 1)
-        curContext = {
-          type: 'pr',
-          context: ctx
-        }
-      } else if (context.payload.issue) {
-        const ctx = await contextHandler.parseIssue(context)
-        if (!ctx) {
-          throw new Error('issue not found on context')
-        }
-        log(`issue context: ${JSON.stringify(ctx)}`, 1)
-
-        curContext = {
-          type: 'issue',
-          context: ctx
-        }
-      } else {
-        log(
-          `There is no context to parse: ${JSON.stringify(context.payload)}`,
-          7
-        )
-        throw new Error('There is no context')
+      log(`PR context: ${JSON.stringify(ctx)}`, 1)
+      curContext = {
+        type: 'pr',
+        context: ctx
       }
+    } else if (context.payload.issue) {
+      const ctx = await contextHandler.parseIssue(context)
+      if (!ctx) {
+        throw new Error('issue not found on context')
+      }
+      log(`issue context: ${JSON.stringify(ctx)}`, 1)
 
-      /**
-       * Syncronise labels to repository
-       * @author IvanFon, TGTGamer, jbinda
-       * @since 1.0.0
-       */
+      curContext = {
+        type: 'issue',
+        context: ctx
+      }
+    } else {
+      log(`There is no context to parse: ${JSON.stringify(context.payload)}`, 7)
+      throw new Error('There is no context')
+    }
+
+    /**
+     * Syncronise labels to repository
+     * @author IvanFon, TGTGamer, jbinda
+     * @since 1.0.0
+     */
+    await labelHandler
+      .syncLabels({
+        client: this.client,
+        repo,
+        config: config.labels,
+        dryRun
+      })
+      .catch((err: { message: string | Error }) => {
+        log(`Error thrown while handling syncLabels tasks: ${err.message}`, 5)
+      })
+
+    // Mapping of label ids to Github names
+    const labelIdToName = await Object.entries(config.labels).reduce(
+      (acc: { [key: string]: string }, cur) => {
+        acc[cur[0]] = cur[1].name
+        return acc
+      },
+      {}
+    )
+
+    /**
+     * Apply labels to context
+     * @author IvanFon, TGTGamer, jbinda
+     * @since 1.0.0
+     */
+    if (curContext.type === 'pr') {
       await labelHandler
-        .syncLabels({
+        .applyPR({
           client: this.client,
+          config: config.pr,
+          labelIdToName,
+          prContext: curContext.context,
           repo,
-          config: config.labels,
           dryRun
         })
         .catch((err: { message: string | Error }) => {
-          log(`Error thrown while handling syncLabels tasks: ${err.message}`, 5)
+          log(`Error thrown while handling PRLabel tasks: ${err.message}`, 5)
         })
-
-      // Mapping of label ids to Github names
-      const labelIdToName = await Object.entries(config.labels).reduce(
-        (acc: { [key: string]: string }, cur) => {
-          acc[cur[0]] = cur[1].name
-          return acc
-        },
-        {}
-      )
-
-      /**
-       * Apply labels to context
-       * @author IvanFon, TGTGamer, jbinda
-       * @since 1.0.0
-       */
-      if (curContext.type === 'pr') {
-        await labelHandler
-          .applyPR({
-            client: this.client,
-            config: config.pr,
-            labelIdToName,
-            prContext: curContext.context,
-            repo,
-            dryRun
-          })
-          .catch((err: { message: string | Error }) => {
-            log(`Error thrown while handling PRLabel tasks: ${err.message}`, 5)
-          })
-      } else if (curContext.type === 'issue') {
-        await labelHandler
-          .applyIssue({
-            client: this.client,
-            config: config.issue,
-            issueContext: curContext.context,
-            labelIdToName,
-            repo,
-            dryRun
-          })
-          .catch((err: { message: string | Error }) => {
-            log(
-              `Error thrown while handling issueLabel tasks: ${err.message}`,
-              5
-            )
-          })
-      }
-    } catch (err) {
-      log(`Error in supper labeler: ` + err.message, 5)
+    } else if (curContext.type === 'issue') {
+      await labelHandler
+        .applyIssue({
+          client: this.client,
+          config: config.issue,
+          issueContext: curContext.context,
+          labelIdToName,
+          repo,
+          dryRun
+        })
+        .catch((err: { message: string | Error }) => {
+          log(`Error thrown while handling issueLabel tasks: ${err.message}`, 5)
+        })
     }
   }
 }
